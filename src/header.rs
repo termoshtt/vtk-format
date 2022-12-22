c1cc4ea3d017d1eff7d4c295f650c4db3a83e556
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-12-22 19:35:03 +0900

    header::Header

--- src/header.rs
+++ src/header.rs
@@ -1,8 +1,25 @@
 use crate::*;
 use nom::{branch::alt, bytes::complete::*, character::complete::*, sequence::tuple, Parser};
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Header {
+    pub version: (u64, u64),
+    pub title: String,
+    pub format: Format,
+}
+
+pub fn header(input: &str) -> Result<Header> {
+    tuple((head_line, multispace1, title, line_ending, format))
+        .map(|(version, _, title, _, format)| Header {
+            version,
+            title: title.to_string(),
+            format,
+        })
+        .parse(input)
+}
+
 /// File header `# vtk DataFile Version x.x`
-pub fn header(input: &str) -> Result<(u64, u64)> {
+pub fn head_line(input: &str) -> Result<(u64, u64)> {
     let (input, _) = tuple((
         char('#'),
         space0,
@@ -45,15 +62,40 @@ pub fn format(input: &str) -> Result<Format> {
 
 #[cfg(test)]
 mod test {
+    use super::{Format, Header};
     use nom::Finish;
 
     #[test]
-    fn header() {
+    fn head_line() {
         assert_eq!(
-            super::header("# vtk DataFile Version 2.0")
+            super::head_line("# vtk DataFile Version 2.0")
                 .finish()
                 .unwrap(),
             ("", (2, 0))
+        );
+    }
+
+    #[test]
+    fn header() {
+        let (residual, out) = super::header(
+            r#"
+            # vtk DataFile Version 2.0
+            Cube example
+            ASCII
+            "#
+            .trim(),
+        )
+        .finish()
+        .unwrap();
+
+        assert_eq!(residual, "");
+        assert_eq!(
+            out,
+            Header {
+                version: (2, 0),
+                title: "Cube example".to_string(),
+                format: Format::ASCII,
+            }
         );
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,57 +7,16 @@ pub mod primitive;
 use header::*;
 use primitive::*;
 
-use nom::{character::complete::*, error::VerboseError, sequence::tuple, Parser};
+use nom::error::VerboseError;
 
 pub type Result<'input, T> = nom::IResult<&'input str, T, VerboseError<&'input str>>;
 
-pub fn line_end(input: &str) -> Result<()> {
-    tuple((space0, line_ending)).map(|_| ()).parse(input)
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Vtk {
-    pub version: (u64, u64),
-    pub title: String,
-    pub format: Format,
+    pub header: Header,
 }
 
 pub fn vtk(input: &str) -> Result<Vtk> {
-    tuple((header, line_end, title, line_end, format))
-        .map(|(version, _, title, _, format)| Vtk {
-            version,
-            title: title.to_string(),
-            format,
-        })
-        .parse(input)
-}
-
-#[cfg(test)]
-mod test {
-    use super::{Format, Vtk};
-    use nom::Finish;
-
-    #[test]
-    fn vtk() {
-        let (residual, out) = super::vtk(
-            r#"
-            # vtk DataFile Version 2.0
-            Cube example
-            ASCII
-            "#
-            .trim(),
-        )
-        .finish()
-        .unwrap();
-
-        assert_eq!(residual, "");
-        assert_eq!(
-            out,
-            Vtk {
-                version: (2, 0),
-                title: "Cube example".to_string(),
-                format: Format::ASCII,
-            }
-        );
-    }
+    let (input, header) = header(input)?;
+    Ok((input, Vtk { header }))
 }
