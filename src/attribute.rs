059bde694407aa199fa27c3fd17b4e2ec67cf82d
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-03-04 16:01:34 +0900

    Blank impls

--- src/attribute.rs
+++ src/attribute.rs
@@ -67,6 +67,24 @@ pub fn scalars(n: usize) -> impl FnMut(&str) -> Result<Scalars> {
     }
 }
 
+/// ```text
+/// COLOR_SCALARS dataName nValues
+/// c00 c01 ... c 0(nValues-1)
+/// c10 c11 ... c 1(nValues-1)
+/// ...
+/// c(n-1)0 c (n-1)1 ... c (n-1)(nValues-1)
+/// ```
+pub struct ColorScalars {}
+pub fn color_scalars(_input: &str) -> Result<ColorScalars> {
+    todo!()
+}
+
+/// ```text
+/// LOOKUP_TABLE tableName size
+/// r0 g0 b0 a0
+/// r1 g1 b1 a1
+/// ...
+/// ```
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct LookupTable {
     table_name: String,
@@ -74,10 +92,6 @@ pub struct LookupTable {
 }
 
 pub fn lookup_table(input: &str) -> Result<LookupTable> {
-    // LOOKUP_TABLE tableName size
-    // r0 g0 b0 a0
-    // r1 g1 b1 a1
-    // ...
     let (input, (_tag, _, table_name, _, size, _)) = tuple((
         tag("LOOKUP_TABLE"),
         multispace1,
@@ -95,6 +109,89 @@ pub fn lookup_table(input: &str) -> Result<LookupTable> {
             colors,
         },
     ))
+}
+
+/// ```text
+/// VECTORS dataName dataType
+/// v0x v0y v0z
+/// v1x v1y v1z
+/// ...
+/// v(n-1)x v(n-1)y v(n-1)z
+/// ```
+pub struct Vectors {}
+pub fn vectors(_input: &str) -> Result<Vectors> {
+    todo!()
+}
+
+/// ```text
+/// NORMALS dataName dataType
+/// n0x n0y n0z
+/// n1x n1y n1z
+/// ...
+/// n(n-1)x n(n-1)y n(n-1)z
+/// ```
+pub struct Normals {}
+pub fn normals(_input: &str) -> Result<Normals> {
+    todo!()
+}
+
+/// ```text
+/// TEXTURE_COORDINATES dataName dim dataType
+/// t00 t01 ... t0(dim-1)
+/// t10 t11 ... t1(dim-1)
+/// ...
+/// t(n-1)0 t(n-1)1 ... t(n-1)(dim-1)
+/// ```
+pub struct TextureCoordinates {}
+pub fn texture_coordinates(_input: &str) -> Result<TextureCoordinates> {
+    todo!()
+}
+
+/// ```text
+/// TENSORS dataName dataType
+/// t000 t001 t002
+/// t010 t011 t012
+/// t020 t021 t022
+///
+/// t100 t101 t102
+/// t110 t111 t112
+/// t120 t121 t122
+///
+/// ...
+///
+/// t(n-1)00 t(n-1)01 t(n-1)02
+/// t(n-1)10 t(n-1)11 t(n-1)12
+/// t(n-1)20 t(n-1)21 t(n-1)22
+/// ```
+pub struct Tensors {}
+pub fn tensors(_input: &str) -> Result<Tensors> {
+    todo!()
+}
+
+/// ```text
+/// FIELD dataName numArrays
+/// arrayName0 numComponents numTuples dataType
+/// f00 f 01 ... f 0(numComponents-1)
+/// f10 f 11 ... f 1(numComponents-1)
+/// ...
+/// f(numTuples-1)0 f(numTuples-1)1 ... f(numTuples-1)(numComponents-1)
+///
+/// arrayName1 numComponents numTuples dataType
+/// f00 f 01 ... f 0(numComponents-1)
+/// f10 f 11 ... f 1(numComponents-1)
+/// ...
+/// f(numTuples-1)0 f(numTuples-1)1 ... f(numTuples-1)(numComponents-1)
+/// ...
+///
+/// arrayName(numArrays-1) numComponents numTuples dataType
+/// f00 f 01 ... f 0(numComponents-1)
+/// f10 f 11 ... f 1(numComponents-1)
+/// ...
+/// f(numTuples-1)0 f(numTuples-1)1 ... f(numTuples-1)(numComponents-1)
+/// ```
+pub struct Field {}
+pub fn field(_input: &str) -> Result<Field> {
+    todo!()
 }
 
 #[cfg(test)]
